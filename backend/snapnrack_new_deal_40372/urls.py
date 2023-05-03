f729741712ee78b0408305cd0d444207e8d796a8
Crowdbotics <team@crowdbotics.com>
2023-05-03 18:17:19 +0000

    Update admin title

--- backend/snapnrack_new_deal_40372/urls.py
+++ backend/snapnrack_new_deal_40372/urls.py
@@ -34,9 +34,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "snapNRack New Deal"
-admin.site.site_title = "snapNRack New Deal Admin Portal"
-admin.site.index_title = "snapNRack New Deal Admin"
+admin.site.site_header = "snapNRack"
+admin.site.site_title = "snapNRack Admin Portal"
+admin.site.index_title = "snapNRack Admin"
 
 # swagger
 urlpatterns += [
